3cc303dbd626794bab9cedec51edf111dca3d80e
Arnav-Jhajharia <jhajhariaarnav@gmail.com>
2025-08-07 02:23:32 +0800

    Initial Commit

---
+++ scat5/ImmersiveView.swift
@@ -0,0 +1,30 @@
+//
+//  ImmersiveView.swift
+//  scat5
+//
+//  Created by Arnav Jhajharia on 7/8/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(immersiveContentEntity)
+
+                // Put skybox here.  See example in World project available at
+                // https://developer.apple.com/
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .full) {
+    ImmersiveView()
+        .environment(AppModel())
+}
